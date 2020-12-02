c2eb278e923cf2811ad07c7740fe9d26cf1be516
Alexander J McKenzie <AMCKEN205@caledonian.ac.uk>
2020-12-02 17:25:08 +0000

    git ignore refreshed cache

---
+++ ClipKeep/Models/Interfaces/IDisplayable.cs
@@ -0,0 +1,12 @@
+﻿namespace ClipKeep.Models.Interfaces
+{
+    /// <summary>
+    /// Defines methods that should exist within model to be stored in the Cosmos DB
+    /// </summary>
+    public interface IDisplayable
+    {
+
+        void Display();
+
+    }
+}
---
+++ ClipKeep/Models/PastedText.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClipKeep.Models.Interfaces;
+using Newtonsoft.Json;
+
+namespace ClipKeep.Models
+{
+    public class PastedText : PastedItem<string>, IDisplayable
+    {
+        public void Display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToJson()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
